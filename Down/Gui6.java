261995be0b4cd72f6669868e8447c21baa1a65df
With_no_reality_whatever <31361803+liuyu4g@users.noreply.github.com>
2018-02-27 15:37:04 +0800

    Add files via upload

---
+++ Down/Gui6.java
@@ -0,0 +1,16 @@
+package com.liuyu.gui;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Gui6 extends JFrame{
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
